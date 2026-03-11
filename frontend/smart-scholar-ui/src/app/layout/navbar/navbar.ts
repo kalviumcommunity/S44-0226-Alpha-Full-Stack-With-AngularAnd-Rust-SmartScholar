2f2de725cd0117d07ea8c540d29f7cb2402eca0b
SomendraZ <somendra.8.1.2003@gmail.com>
2026-03-11 15:53:44 +0530

    feat(auth): implement Angular AuthService and route guard

--- frontend/smart-scholar-ui/src/app/layout/navbar/navbar.ts
+++ frontend/smart-scholar-ui/src/app/layout/navbar/navbar.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  constructor(
+    private authService: Auth,
+    private router: Router,
+  ) {}
 
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
