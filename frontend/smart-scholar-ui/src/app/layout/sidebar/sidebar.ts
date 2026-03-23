c28d4edf4fd58b8a792bad1c48a462cfaa761715
SomendraZ <somendra.8.1.2003@gmail.com>
2026-03-23 15:43:22 +0530

    [Sprint-2] fix: add form validation and error messages to application form - Alpha

--- frontend/smart-scholar-ui/src/app/layout/sidebar/sidebar.ts
+++ frontend/smart-scholar-ui/src/app/layout/sidebar/sidebar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Auth } from '../../services/auth';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
